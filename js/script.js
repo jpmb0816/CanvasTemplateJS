c85f63c9257ecd96d12c38f91cf4591b2f922df3
JP Beyong <johnpaulo.beyong0816@gmail.com>
2021-01-02 14:33:39 +0800

    Initial commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2d');
 const FPS = 60;
 
 function render() {
-	ctx.fillStyle = '#1f2833';
+	ctx.fillStyle = '#5e1a31';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
